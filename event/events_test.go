2b75ae577e777295e8170bbae19562b82cbb50aa
Erdem Gezer <erdemgezer@gmail.com>
2015-11-17 16:10:15 +0000

    Go fmt

--- event/events_test.go
+++ event/events_test.go
@@ -1,21 +1,21 @@
 package event_test
 
 import (
-  "testing"
 	. "github.com/reevoo/tracker/Godeps/_workspace/src/github.com/smartystreets/goconvey/convey"
 	. "github.com/reevoo/tracker/event"
+	"testing"
 )
 
 func TestEvents(t *testing.T) {
 
-      Convey("Generating an ID", t, func() {
+	Convey("Generating an ID", t, func() {
 		event := New(map[string][]string{})
 		So(event["id"], ShouldNotBeNil)
 	})
 
-        Convey("An Empty Event", t, func() {
-          event := New(map[string][]string{})
-          So(event.Empty(), ShouldBeTrue)
-        })
+	Convey("An Empty Event", t, func() {
+		event := New(map[string][]string{})
+		So(event.Empty(), ShouldBeTrue)
+	})
 
 }
